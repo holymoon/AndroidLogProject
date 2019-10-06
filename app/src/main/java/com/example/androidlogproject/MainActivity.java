2211a5f6ad507d606c56462eba145be463183ee6
Houra <houra.mehdipour@gmail.com>
2019-10-06 10:07:20 +0330

    some changes in mainActivity

--- app/src/main/java/com/example/androidlogproject/MainActivity.java
+++ app/src/main/java/com/example/androidlogproject/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.androidlogproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("TAG","in onCreate");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("TAG","in onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("TAG","in onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("TAG","in onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("TAG","in onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("TAG","in onDestroy");
     }
 }
